f6c93e2aedf06f32ab489c47052c35558f218b0e
bahaa <bahaaabdelrahman11@gmail.com>
2025-07-06 18:21:23 +0300

    my new app

---
+++ src/app/features/products/products.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsRoutingModule } from './products-routing.module';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { SharedModule } from '../../shared/shared.module';
+import { TranslateModule } from '@ngx-translate/core';
+
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+    ProductCardComponent
+  ],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    SharedModule ,
+    TranslateModule
+  ]
+})
+export class ProductsModule { }
